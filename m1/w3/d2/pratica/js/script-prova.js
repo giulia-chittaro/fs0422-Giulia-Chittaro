e8cfafdf8716a029311ebdb95c85f56677a73273
giulia-chittaro <chittaro.giulia@gmail.com>
2022-07-05 17:35:58 +0200

    esercizi1

--- m1/w3/d2/pratica/js/script-prova.js
+++ m1/w3/d2/pratica/js/script-prova.js
@@ -2,10 +2,10 @@ let nome = "Giulia"
 console.log(nome)
 let cognome = "Chittaro"
 console.log(cognome)
-
 console.log("nome utente:" + nome + ", cognome utente:" + cognome)
 
-
+const d = 2;
+console.log(d);
 
 let a = 10;
 let b = a + 30;
@@ -13,8 +13,11 @@ let c = 20 + 40;
 let somma = a + b + c
 console.log(somma)
 
-const d = 2;
-console.log(d);
+let g = 10
+let f = 20
+h = console.log( g + f )   
+
+
 
 const obj = {
     nome: '',
@@ -23,4 +26,27 @@ const obj = {
 
 obj.nome = 'Giulia';
 console.log(obj);
+
+
+let saluto = 'Ciao';
+ let salut1 = 'Hi';
+console.log(a != b)
+console.log(a !== b)
+
+
+
+var button = document.querySelector('#button');
+console.log(button);
+var heading = document.querySelector('#heading');
+console.log(heading);
+
+button.onclick = () => {
+    const saluto = prompt('Sei arrivato?');
+    alert(`Ciao Lidia , nice to see you!`);
+    heading.textContent = `Qual è il tuo animale preferito?`;
+  }
+
+
+
+
 
