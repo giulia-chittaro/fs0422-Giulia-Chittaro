7e133f7c25a20b6b11b983ccdcf66a231cadef77
giulia-chittaro <107913521+giulia-chittaro@users.noreply.github.com>
2022-07-18 16:17:58 +0200

    pratica

---
+++ m2/w1/d1/pratica/script.js
@@ -0,0 +1,74 @@
+
+let data = new Date()
+
+// metodo 1
+
+data.toLocaleDateString()
+console.log(data)
+
+
+let dataAttuale = document.getElementById('data-attuale')
+dataAttuale.innerHTML = data
+
+
+// metodo 2
+
+
+let dataAttuale2 = document.getElementById('data-attuale2')
+dataAttuale2.innerHTML = data.toLocaleDateString()
+
+
+
+// mostrare ogni elemento
+
+console.log(data.getDate())
+console.log(data.getMonth())
+console.log(data.getFullYear())
+console.log(data.getHours())
+console.log(data.getMinutes())
+console.log(data.getSeconds())
+
+
+
+//mostare ora in tempo reale
+
+setInterval(function() {
+    
+ let time = new Date()
+ 
+ let clock = document.getElementById('clock')
+ clock.innerHTML = time.toLocaleTimeString()   
+})
+
+
+
+// data formattazione europea
+
+let dataEuropea = new Date (2022, 6 , 18 , 15, 56, 15, 1)
+console.log(dataEuropea)
+
+let dataFormattata = document.getElementById('data-europea')
+dataFormattata.innerHTML = dataEuropea
+
+
+// prove
+
+console.log (data.getDay())
+
+
+if(data.getDay() == 0){
+
+    console.log('Domenica')
+} else if ( data.getDay() == 1) {
+    console.log('Lunedì')
+} else if ( data.getDay() == 2) {
+    console.log('Martedì')
+} else if ( data.getDay() == 3) {
+    console.log('Mercoledì')
+} else if ( data.getDay() == 4) {
+    console.log('Giovedì')
+} else if ( data.getDay() == 5) {
+    console.log('Venerdì')
+} else if ( data.getDay() == 6) {
+    console.log('Sabato')
+} 
