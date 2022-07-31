7c849cf3fad5aa277dfb191f3727f825ef38b93a
giulia-chittaro <107913521+giulia-chittaro@users.noreply.github.com>
2022-08-01 01:36:27 +0200

    progetto finale

--- m2/w2/progetto/script.js
+++ m2/w2/progetto/script.js
@@ -1,41 +1,31 @@
-function getTimeRemaining(endtime) {
-    var t = Date.parse(endtime) - Date.parse(new Date());
-    var seconds = Math.floor((t / 1000) % 60);
-    var minutes = Math.floor((t / 1000 / 60) % 60);
-    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-    var days = Math.floor(t / (1000 * 60 * 60 * 24));
-    return {
-      'total': t,
-      'days': days,
-      'hours': hours,
-      'minutes': minutes,
-      'seconds': seconds
-    };
+
+var countDownDate = new Date("February 18, 2023 20:30:00").getTime();
+
+var x = setInterval(function() {
+
+  var now = new Date().getTime();
+
+  var contoRovescia = countDownDate - now;
+
+  var days = Math.floor(contoRovescia / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((contoRovescia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((contoRovescia % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((contoRovescia % (1000 * 60)) / 1000);
+
+  
+  document.getElementById("days").innerHTML = days + "d " ;
+
+  document.getElementById("hours").innerHTML =  hours + "h";
+
+  document.getElementById("minutes").innerHTML = minutes + "m" ;
+
+  document.getElementById("seconds").innerHTML = seconds + "s";
+
+
+
+
+  if (contoRovescia < 0) {
+    clearInterval(x);
+    document.getElementById("clockDiv").innerHTML = "We are waiting for you!";
   }
-  
-  function initializeClock(id, endtime) {
-    var clock = document.getElementById(id);
-    var daysSpan = clock.querySelector('.days');
-    var hoursSpan = clock.querySelector('.hours');
-    var minutesSpan = clock.querySelector('.minutes');
-    var secondsSpan = clock.querySelector('.seconds');
-  
-    function updateClock() {
-      var t = getTimeRemaining(endtime);
-  
-      daysSpan.innerHTML = t.days;
-      hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-      minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-      secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-  
-      if (t.total <= 0) {
-        clearInterval(timeinterval);
-      }
-    }
-  
-    updateClock();
-    var timeinterval = setInterval(updateClock, 1000);
-  }
-  
-  var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
-  initializeClock('clockdiv', deadline);
+}, 1000);
